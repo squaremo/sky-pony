af01ea108f7eaedbe332ba1b00015fc77e57a96b
Michael Bridgen <mikeb@squaremobius.net>
2012-01-31 16:19:16 +0000

    This looks a bit better

--- js/index.js
+++ js/index.js
@@ -5,23 +5,21 @@ model = {'columnTitle': "RabbitMQ health"};
 var meters = model.meters = [];
 
 var memory = ko.observable(0);
-meters.push(new Meter(memory, {'unit': '% memory', 'hwm': 50}));
+meters.push(new Meter(memory, {'unit': '% memory',
+                               'precision': 1,
+                               'hwm': 50}));
 
 var mps = ko.observable(0);
-meters.push(new Meter(mps, {'unit': 'msg / sec'}));
+meters.push(new Meter(mps, {'unit': 'msg / sec',
+                            'precision': 0}));
 
 var messages_window = new SlidingWindow(3);
+var big_messages_window = new SlidingWindow(20);
 meters.push(new Meter(messages_window.average,
                       {'unit': 'moving avg total msgs',
+                       'precision': 0,
                        'hwm': 100000,
-                       'sparkline': messages_window.samples}));
-
-// this probably won't work. bloody floats.
-function decimalplaces(val, digits) {
-  var mult = Math.pow(10, digits);
-  var rounded = Math.round(val * mult);
-  return rounded / mult;
-}
+                       'sparkline': big_messages_window.samples}));
 
 ko.bindingHandlers.sparkline = {
   init: function(element, valueAccessor, _allBindingsAccessor) {
@@ -30,12 +28,16 @@ ko.bindingHandlers.sparkline = {
   },
   update: function(element, valueAccessor, _allBindingsAccessor) {
     console.log(valueAccessor());
-    $(element).sparkline(valueAccessor(), {'width': '100%',
-                                           'height': '50px',
-                                           'chartRangeMin': 0,
-                                           'spotColor': '#fff',
-                                           'lineColor': '#666',
-                                           'fillColor': '#ccc'});
+    $(element).sparkline(valueAccessor(), {
+      'type': 'bar',
+      'width': '100%',
+      'height': '80px',
+      'chartRangeMin': 0,
+      'spotColor': '#fff',
+      'lineColor': '#666',
+      'barColor': '#ccc',
+      'barWidth': 14,
+      'fillColor': '#ccc'});
   }
 }
 
@@ -52,5 +54,6 @@ socket.onmessage = function(msg) {
   else if (event.overview) {
     mps(decimalplaces(event.overview.message_stats.publish_details.rate, 1));
     messages_window.push(event.overview.queue_totals.messages);
+    big_messages_window.push(event.overview.queue_totals.messages);
   }
 };
